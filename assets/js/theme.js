8767a02a0d2102b563852b445a612bda2016d367
Daniel <danuvrabii@gmail.com>
2024-03-17 23:17:03 +0200

    main image modifications + theme

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,6 +1,6 @@
 export function setTheme(theme) {
     if (theme === 'light') {
-        document.body.style.background = 'linear-gradient(to right, #a5a5a5, #ffffff)';
+        document.body.style.background = '#a5a5a5';
         document.body.style.color = '#000000'; // Черный текст для светлой темы
     } else if (theme === 'dark') {
         document.body.style.background = 'linear-gradient(to right, #0D0D0D, #1a1a1a)';
